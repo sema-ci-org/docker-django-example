a0b72a9ed9e87d1475bc410b2451c665131dd265
Girish Koliki <girish.koliki+ci@semasoftware.com>
2024-09-26 14:17:27 +0100

    creating app

--- src/config/settings.py
+++ src/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = list(map(str.strip, allowed_hosts.split(",")))
 # Application definitions
 INSTALLED_APPS = [
     "pages.apps.PagesConfig",
+    "accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
